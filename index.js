aabe3206e3819e3a58ba029338a218b947ad08d9
KelwinRibeiro <132370814+KelwinRibeiro@users.noreply.github.com>
2023-08-14 23:27:11 +0000

    TESTE

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+
+const server = http.createServer((req,res)=>{
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+
+    res.end("FHSADHASDHAHDSA");
+
+});
+
+
+server.listen(port,hostname,()=>{
+    console.log("SERVER RODANDO");
+
+})
